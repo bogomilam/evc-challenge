50bcf5d9f2ff0a86a4a96ced941240ad9722db91
Bogomil Mihaylov <bogomilmihaylov@Bogomils-MacBook-Pro.local>
2023-12-05 16:10:24 +0000

    inital commit with chakra and theme

---
+++ src/app/styling/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    // brand: {
+      100: "#f7fafc",
+      'light-green': "#00FF00",
+      // ...
+      900: "#1a202c",
+    // },
+  },
+  // fonts,
+  breakpoints: {
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    xl: "80em",
+  },
+});
